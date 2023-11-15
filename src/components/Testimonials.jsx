cc8692cb82e992c6de6d839d4b0cb67e82d4224b
TatsuKenshi <rockinbg@gmail.com>
2023-11-15 20:24:23 +0100

    test mongoDB connection - successful

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  const [items, setItems] = useState([]);
+
+  const getAllItems = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/api/v1/tasks");
+
+      const data = response.data.tasks;
+      console.log(data);
+      setItems(data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getAllItems();
+  }, []);
+
+  return (
+    <div>
+      {items.map((item) => {
+        const id = item._id;
+        return <p key={id}>{item.name}</p>;
+      })}
+    </div>
+  );
 };
 
 export default Testimonials;
